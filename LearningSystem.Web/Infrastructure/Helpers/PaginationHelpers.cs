b934860efca434bb1e4c2bf925e651482207c042
Anna Stambolieva <astambi@users.noreply.github.com>
2019-07-17 13:17:45 +0300

    Added HomeController unit tests

--- LearningSystem.Web/Infrastructure/Helpers/PaginationHelpers.cs
+++ LearningSystem.Web/Infrastructure/Helpers/PaginationHelpers.cs
@@ -2,7 +2,7 @@
 {
     using System;
 
-    public static class PaginationHelpers
+    public class PaginationHelpers
     {
         public static int GetTotalPages(int itemsCount, int pageSize = WebConstants.PageSize)
             => Math.Max(1, (int)Math.Ceiling(itemsCount / (double)pageSize));
